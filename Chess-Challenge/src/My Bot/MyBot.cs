bfc8e768b7e6528003dff57a4c92f8adce986458
Ezdies <bvbclubbvb@gmail.com>
2023-07-22 17:42:01 +0200

    my first attempt to improve the bot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,24 @@
-﻿using ChessChallenge.API;
+﻿using System.Collections.Generic;
+using System.Linq;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
+    private Dictionary<PieceType, int> pieceRatings = new Dictionary<PieceType, int>()
+    {
+        { PieceType.Pawn, 1 },
+        { PieceType.Knight, 3 },
+        { PieceType.Bishop, 3 },
+        { PieceType.Rook, 5 },
+        { PieceType.Queen, 9 },
+        { PieceType.King, int.MaxValue }
+    };
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move[] firstMoves = board.GetLegalMoves()
+                                    .Where(move => move.MovePieceType == PieceType.Pawn).Take(4).ToArray();
+        Move[] movesThatCapture = board.GetLegalMoves().Where(move => move.IsCapture).ToArray();
+
+        return movesThatCapture.Length > 0 ? movesThatCapture[0] : firstMoves[0];
     }
 }
